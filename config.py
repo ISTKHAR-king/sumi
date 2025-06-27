84e6498fedc80112857b2bdc1bedb664fde46e2b
SonkarXKoushal <sonkarp463@gmail.com>
2025-06-27 17:39:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -90,16 +90,16 @@ clean = {}
 autoclean = []
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://files.catbox.moe/m9vjlg.jpg"
+    "START_IMG_URL", "https://files.catbox.moe/n4vivz.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://graph.org/file/f16f757fe9c10f81b67ef.jpg"
+    "PING_IMG_URL", "https://files.catbox.moe/8kifut.jpg"
 )
-PLAYLIST_IMG_URL = "https://te.legra.ph/file/e68f585a0a2e4e5dc1164.jpg"
-STATS_IMG_URL = "https://te.legra.ph/file/4a7c28726502e24ea0fe0.jpg"
-TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/810f874873e1565cf5732.jpg"
-TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/16d7dd76f4ce8b8b01fdf.jpg"
-STREAM_IMG_URL = "https://te.legra.ph/file/53f1a295e172d39eaa39d.jpg"
+PLAYLIST_IMG_URL = "https://files.catbox.moe/2vq8oz.jpg"
+STATS_IMG_URL = "https://files.catbox.moe/8kifut.jpg"
+TELEGRAM_AUDIO_URL = "https://files.catbox.moe/2vq8oz.jpg"
+TELEGRAM_VIDEO_URL = "https://files.catbox.moe/2vq8oz.jpg"
+STREAM_IMG_URL = "https://files.catbox.moe/2vq8oz.jpg"
 SOUNCLOUD_IMG_URL = "https://te.legra.ph/file/bb0ff85f2dd44070ea519.jpg"
 YOUTUBE_IMG_URL = "https://te.legra.ph/file/53f1a295e172d39eaa39d.jpg"
 SPOTIFY_ARTIST_IMG_URL = "https://te.legra.ph/file/5d90c3bc7f0d229194a9f.jpg"
