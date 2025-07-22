f079a5fd2613de1e990f5706f305275119c5282f
SonkarXKoushal <sonkarp463@gmail.com>
2025-07-22 09:57:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -54,7 +54,7 @@ SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID", None)
 SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET", None)
 
 YTPROXY_URL = getenv("YTPROXY_URL", 'https://tgapi.xbitcode.com') ## E.G https://yt.okflix.
-YT_API_KEY = "xbit_0000743191821303754064"
+YT_API_KEY = "xbit_8OLNDEWF71UBS21ABH8MVJ"
 COOKIES_URL=getenv("COOKIES_URL" , "https://gist.githubusercontent.com/sparrow9616/f29fc6588086a3c72d92dd9c03773350/raw/4229f3f4aab4a6693fc0794d136d30f54d67ae85/gistfile1.txt")
 
 API_URL = getenv("API_URL", 'https://api.thequickearn.xyz') #youtube song url
