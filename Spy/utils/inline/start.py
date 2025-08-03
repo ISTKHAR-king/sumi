faea3e361444f798098bb72cf55d3bce7ed8475c
SonkarXKoushal <sonkarp463@gmail.com>
2025-08-04 00:16:56 +0530

    Update start.py

--- Spy/utils/inline/start.py
+++ Spy/utils/inline/start.py
@@ -32,7 +32,7 @@ def private_panel(_):
         [
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
         ],
-        [InlineKeyboardButton(text="📱 Movies+Web series+Anime 🦋", url=f"https://t.me/+5Hsgtp8rV81lMTc1")],
+        [InlineKeyboardButton(text="📱 Movies+Web series+Anime 🦋", url=f"https://get2short.com/FxVnA")],
     
     ]
     
