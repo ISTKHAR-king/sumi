c2cd7bd807a2f91efc81a64a938e4684fca5916d
SonkarXKoushal <sonkarp463@gmail.com>
2025-08-15 11:18:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -53,9 +53,9 @@ AUTO_SUGGESTION_TIME = int(
 SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID", None)
 SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET", None)
 
-API_URL = getenv("API_URL", 'https://api.thequickearn.xyz') #youtube song url
-VIDEO_API_URL = getenv("VIDEO_API_URL", 'https://api.video.thequickearn.xyz')
-API_KEY = getenv("API_KEY", 'NxGBNexGenBotsdd043c') # 
+YOUR_API_URL = getenv("YOUR_API_URL", "http://46.38.237.240:8000")
+YOUR_API_KEY = getenv("YOUR_API_KEY", "strangerapic6633fa07")
+
 
 
 PLAYLIST_FETCH_LIMIT = int(getenv("PLAYLIST_FETCH_LIMIT", 25))
