34de24e214f07894ec6dcd21068aa7120a2a3391
SonkarXKoushal <sonkarp463@gmail.com>
2025-08-27 08:20:30 +0530

    Update Youtube.py

--- Spy/platforms/Youtube.py
+++ Spy/platforms/Youtube.py
@@ -14,7 +14,7 @@ from Spy.utils.formatters import time_to_seconds
 
 # ============== CONFIGURE YOUR API ==============
 YOUR_API_URL = "http://46.38.237.240:8000"
-YOUR_API_KEY = "strangerapi6dc28f31a"            # <--- Change me!
+YOUR_API_KEY = "strangerapi3106441b0"            # <--- Change me!
 # ================================================
 
 async def get_file_from_api(video_id, audio=True):
