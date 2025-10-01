91e742f0115b645ac29226b4a6f09bcd19c789e8
KingOfMusicBot <teamriya84@gmail.com>
2025-10-01 18:19:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Sonkarxkoushal/rzx",
+    "https://github.com/KingOfMusicBot/rzx",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "SPY")
 GIT_TOKEN = getenv(
