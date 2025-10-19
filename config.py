d8e5bc31acb8d612d181a2c4aea9df313c79ca2f
KingOfMusicBot <teamriya84@gmail.com>
2025-10-19 17:30:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -54,7 +54,7 @@ SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID", None)
 SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET", None)
 
 YTPROXY_URL = getenv("YTPROXY_URL", 'https://tgapi.xbitcode.com') ## E.G https://yt.okflix.
-YT_API_KEY = "xbit_UH3H68KMZRQF1UJTSCGLYS"
+YT_API_KEY = "xbit_8WBCRMOOEWUI80HIRAIXVR"
 COOKIES_URL=getenv("COOKIES_URL" , "https://gist.githubusercontent.com/sparrow9616/f29fc6588086a3c72d92dd9c03773350/raw/4229f3f4aab4a6693fc0794d136d30f54d67ae85/gistfile1.txt")
 
 PLAYLIST_FETCH_LIMIT = int(getenv("PLAYLIST_FETCH_LIMIT", 25))
