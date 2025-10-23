c3e458e7b531a899fa390938a676d8dc10f1e52c
KingOfMusicBot <teamriya84@gmail.com>
2025-10-23 20:54:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -33,6 +33,10 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
+API_URL = getenv("API_URL", 'https://api.thequickearn.xyz') #youtube song url
+VIDEO_API_URL = getenv("VIDEO_API_URL", 'https://api.video.thequickearn.xyz')
+API_KEY = getenv("API_KEY", 'NxGBNexGenBots0bf9a0'
+
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
     "https://github.com/KingOfMusicBot/rzx",
