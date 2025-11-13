1ede6a338340c5b791478d16186c32c9102d349a
KingOfMusicBot <teamriya84@gmail.com>
2025-11-14 00:53:00 +0530

    Update config.py

--- config.py
+++ config.py
@@ -34,7 +34,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 API_BASE_URL = "http://165.22.220.188:8000"
-API_KEY = os.getenv("YT_API_KEY", "strangerapi2736ade96")
+API_KEY = os.getenv("API_KEY", "strangerapi2736ade96")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
