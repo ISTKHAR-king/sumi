07f9ce2276416ac4ea657689ee9a061982dedd6d
ISTKHAR-king <istkharalam225@gmail.com>
2025-12-05 00:09:14 +0530

    Update start.py

--- Spy/utils/inline/start.py
+++ Spy/utils/inline/start.py
@@ -32,7 +32,7 @@ def private_panel(_):
         [
             InlineKeyboardButton(text=_["S_B_2"], url=config.SUPPORT_CHAT),
         ],
-        [InlineKeyboardButton(text="📱 Movies+Web series+Anime 🦋", url=f"https://get2short.com/FxVnA")],
+        [InlineKeyboardButton(text="📱 Movies+Web series+Anime 🦋", url=f"https://t.me/iamIstkhar")],
     
     ]
     
