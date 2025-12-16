59e14abd882cc285cf097e6868a9b57efb7195fe
ISTKHAR-king <istkharalam225@gmail.com>
2025-12-16 17:33:10 +0530

    Update play.py

--- Spy/utils/inline/play.py
+++ Spy/utils/inline/play.py
@@ -1,6 +1,4 @@
 import math
-from Spy import app
-import config
 
 from pyrogram.types import InlineKeyboardButton
 
@@ -35,40 +33,43 @@ def stream_markup_timer(_, chat_id, played, dur):
     percentage = (played_sec / duration_sec) * 100
     umm = math.floor(percentage)
     if 0 < umm <= 10:
-        bar = "◉—————————"
+        bar = "♡╌╌╌╌╌╌╌╌"
     elif 10 < umm < 20:
-        bar = "—◉————————"
+        bar = "╌♡╌╌╌╌╌╌╌"
     elif 20 <= umm < 30:
-        bar = "——◉———————"
+        bar = "╌╌♡╌╌╌╌╌╌"
     elif 30 <= umm < 40:
-        bar = "———◉——————"
+        bar = "╌╌╌♡╌╌╌╌╌"
     elif 40 <= umm < 50:
-        bar = "————◉—————"
+        bar = "╌╌╌╌♡╌╌╌╌"
     elif 50 <= umm < 60:
-        bar = "—————◉————"
+        bar = "╌╌╌╌╌♡╌╌╌"
     elif 60 <= umm < 70:
-        bar = "——————◉———"
+        bar = "╌╌╌╌╌╌♡╌╌"
     elif 70 <= umm < 80:
-        bar = "———————◉——"
+        bar = "╌╌╌╌╌╌╌♡╌"
     elif 80 <= umm < 95:
-        bar = "————————◉—"
+        bar = "╌╌╌╌╌╌╌╌♡╌"
     else:
-        bar = "—————————◉"
+        bar = "╌╌╌╌╌╌╌╌╌♡"
     buttons = [
-                [
+        [
             InlineKeyboardButton(
                 text=f"{played} {bar} {dur}",
                 callback_data="GetTimer",
             )
         ],
-		[
-         InlineKeyboardButton(text=_["S_B_3"], url=f"https://t.me/{app.username}?startgroup=true",)
+        [
+            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
         [
-         InlineKeyboardButton(text="• ᴏᴡɴᴇʀ •", url="https://t.me/KRITI_UPDATE"),
-         InlineKeyboardButton(text="• ɢʀᴏᴜᴘ •", url="https://t.me/TEAM_BADNAM_BOTS"),
+            InlineKeyboardButton(text="ʀᴇᴘʟᴀʏ[↻]", callback_data=f"ADMIN Replay|{chat_id}"),
+            InlineKeyboardButton(text="ᴄʟᴏsᴇ[✗]", callback_data="close"
+            )
         ],
-        [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
     return buttons
 
@@ -76,13 +77,15 @@ def stream_markup_timer(_, chat_id, played, dur):
 def stream_markup(_, chat_id):
     buttons = [
         [
-         InlineKeyboardButton(text="• ᴏᴡɴᴇʀ •", url="https://t.me/KRITI_UPDATE"),
-         InlineKeyboardButton(text="• ɢʀᴏᴜᴘ •", url="https://t.me/TEAM_BADNAM_BOTS"),
+            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text="II", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text="↻", callback_data=f"ADMIN Replay|{chat_id}"),
+            InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
     ]
     return buttons
-
 
 
 def playlist_markup(_, videoid, user_id, ptype, channel, fplay):
@@ -90,11 +93,11 @@ def playlist_markup(_, videoid, user_id, ptype, channel, fplay):
         [
             InlineKeyboardButton(
                 text=_["P_B_1"],
-                callback_data=f"ALPHAPlaylists {videoid}|{user_id}|{ptype}|a|{channel}|{fplay}",
+                callback_data=f"SagarPlaylists {videoid}|{user_id}|{ptype}|a|{channel}|{fplay}",
             ),
             InlineKeyboardButton(
                 text=_["P_B_2"],
-                callback_data=f"ALPHAPlaylists {videoid}|{user_id}|{ptype}|v|{channel}|{fplay}",
+                callback_data=f"SagarPlaylists {videoid}|{user_id}|{ptype}|v|{channel}|{fplay}",
             ),
         ],
         [
@@ -154,4 +157,3 @@ def slider_markup(_, videoid, user_id, query, query_type, channel, fplay):
         ],
     ]
     return buttons
-       
