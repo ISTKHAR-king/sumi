a84140db880dd2bd10d29bd0bf1a9fcb5fbfcc8d
ISTKHAR-king <istkharalam225@gmail.com>
2025-12-16 13:25:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ SONG_DOWNLOAD_DURATION = int(
     getenv("SONG_DOWNLOAD_DURATION_LIMIT", "54000")
 )
 
-LOGGER_ID = int(getenv("LOGGER_ID", ))
+LOGGER_ID = int(getenv("LOGGER_ID", "" ))
 
 OWNER_ID = int(getenv("OWNER_ID", ""))
 
